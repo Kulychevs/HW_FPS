749615c5a8f673a126b80eededaa7f85019618f4
KulS <arfio11@gmail.com>
2020-04-11 11:54:08 +0300

    lesson 6

---
+++ scripts_Kulychev/MiniMapController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+
+namespace ARPGFrame
+{
+    class MiniMapController : BaseController, IInitialization, IExecuteLate
+    {
+        #region Fields
+
+        private const int UNIT_LAYER = 9;
+
+        private MiniMap _miniMap;
+        private Radar _radar;
+        private Transform _player;               
+        private TeamColor _playerTeamColor;
+        private LayerMask _mask;
+        private float _radarRadius = 10;
+
+        #endregion
+
+
+        #region Methods
+
+        private void DetectRadarObjects()
+        {
+            var units = Physics.OverlapSphere(_player.position, _radarRadius, _mask);
+            foreach (var unit in units)
+            {
+                if (unit.TryGetComponent<BaseEnemy>(out var enemy))
+                {
+                    if (enemy.StateBot != StateBot.Inspection)
+                    {
+                        var isAlly = _playerTeamColor == enemy.GetTeam();                        
+                        _radar.AddObject(enemy, isAlly);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+
+        #region IExecuteLate
+
+        public void ExecuteLate()
+        {
+            if (Time.frameCount % 2 == 0)
+            {
+                _miniMap.Tick(_player);
+                if (Time.frameCount % 60 == 0)
+                    _radar.RemoveObjects();
+                DetectRadarObjects();
+                _radar.DrawRadarDots(_player);
+            }
+        }
+
+        #endregion
+
+
+        #region IInitialization
+
+        public void Initialization()
+        {
+            _mask = 1 << UNIT_LAYER;
+            var player = Object.FindObjectOfType<PlayerUnit>();
+            _player = player.transform;
+            _playerTeamColor = player.GetTeam();
+            _miniMap = Object.FindObjectOfType<MiniMap>();
+            _radar = Object.FindObjectOfType<Radar>();
+
+            _miniMap.Initialization();
+            _radar.Initialization();
+        }
+
+        #endregion
+    }
+}
---
+++ scripts_Kulychev/Radar.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+namespace ARPGFrame
+{
+	public class Radar : MonoBehaviour
+	{
+        #region Fields
+
+        private float mapScale = 4;
+		private Dictionary<BaseEnemy, Image> _radarObjects;
+
+        #endregion
+
+
+        #region Methods
+        public void Initialization()
+		{
+			_radarObjects = new Dictionary<BaseEnemy, Image>();
+		}
+
+		public void AddObject(BaseEnemy unit, bool isAlly)
+		{
+			if (!_radarObjects.ContainsKey(unit))
+			{
+				Image image;
+				if (isAlly)
+					image = ImagePoolLocator.Resolve<Image>(ImageType.Ally).GetObject();
+				else
+					image = ImagePoolLocator.Resolve<Image>(ImageType.Enemy).GetObject();
+				image.enabled = true;
+				_radarObjects.Add(unit, image);
+			}
+		}
+
+		public void RemoveObjects()
+		{
+			foreach (var radObject in _radarObjects)
+			{
+				radObject.Value.enabled = false;
+				if (radObject.Value.color == Color.red)
+					ImagePoolLocator.Resolve<Image>(ImageType.Enemy).PutObject(radObject.Value);
+				else
+					ImagePoolLocator.Resolve<Image>(ImageType.Ally).PutObject(radObject.Value);
+			}
+				_radarObjects.Clear();
+		}
+
+		public void DrawRadarDots(Transform player)
+		{
+			foreach (var radObject in _radarObjects)
+			{
+				if (radObject.Key)
+				{
+					Vector3 radarPos = (radObject.Key.transform.position - player.position);
+
+					float distToObject = Vector3.Distance(player.position, radObject.Key.transform.position) * mapScale;
+
+					float deltay = Mathf.Atan2(radarPos.x, radarPos.z) * Mathf.Rad2Deg - 270 - player.eulerAngles.y;
+
+					radarPos.x = distToObject * Mathf.Cos(deltay * Mathf.Deg2Rad) * -1;
+					radarPos.z = distToObject * Mathf.Sin(deltay * Mathf.Deg2Rad);
+
+					radObject.Value.transform.SetParent(transform);
+
+					radObject.Value.transform.position = new Vector3(radarPos.x, radarPos.z, 0) + transform.position;
+				}
+			}
+		}
+
+        #endregion
+    }
+}
